1f0a69373d62afdc7eb6fe1cbb5194ff061ba597
PetrosKarampas <petros6_97@yahoo.gr>
2021-03-05 00:13:47 +0200

    expand lex

--- Cimple.py
+++ Cimple.py
@@ -1,22 +1,40 @@
 import sys
 
-#                                #
-# ------------Globals------------#
-#                                #
-input_file = "Empty"
+# ---------------------------------------#
+# -          Global Variables           -#
+# ---------------------------------------#
+input_file = None
 char_number = 0
 line_number = 0
 
 
 class Token:
-    def __init__(self, token_type, token_string, token_line_number, token_char_number):
-        self.tk_type = token_type
-        self.tk_string = token_string
-        self.tk_line_number = token_line_number
-        self.tk_char_number = token_char_number
+    def __init__(self, tk_type=None, tk_string=None, tk_line_number=None, tk_char_number=None):
+        self.tk_type = tk_type
+        self.tk_string = tk_string
+        self.tk_line_number = tk_line_number
+        self.tk_char_number = tk_char_number
 
-    #def set_token_type(self):
+    def set_token_type(self, tk_type):
+        self.tk_type = tk_type
 
+    def get_token_type(self):
+        return self.tk_type
+
+    def set_token_string(self, tk_string):
+        self.tk_string = tk_string
+
+    def get_token_string(self):
+        return self.tk_string
+
+    def set_token_line_number(self, tk_line_number):
+        self.tk_line_number = tk_line_number
+
+    def get_token_line_number(self):
+        return self.tk_line_number
+
+    def set_token_char_number(self, tk_char_number):
+        self.tk_char_number = tk_char_number
 
 
 #                                 #
@@ -33,7 +51,7 @@ class TokenType:
     MINUS_TK = 3
     TIMES_TK = 4
     DIV_TK = 5
-    # Interellatiom Opperators
+    # Interpellation Operators
     LESS_TK = 6
     GREATER_TK = 7
     EQUAL_TK = 8
@@ -42,18 +60,18 @@ class TokenType:
     NOT_EQUAL = 11
     # Value Assignment
     ASSIGN_TK = 12
-    # Panctuation Points
+    # Punctuation Points
     SEMI_COLON_TK = 13
     COMMA_TK = 14
     COLON_TK = 15
     PERIOD_TK = 16
     # Grouping Symbols
-    OPEN_BRACKET_TK = 17
-    CLOSE_BRACKET_TK = 18
+    OPEN_SQUARE_BRACKET_TK = 17
+    CLOSE_SQUARE_BRACKET_TK = 18
     OPEN_PARENTHESIS_TK = 19
     CLOSE_PARENTHESIS_TK = 20
-    OPEN_BRACES_TK = 21
-    CLOSE_BRACES_TK = 22
+    OPEN_CURLY_BRACKET_TK = 21
+    CLOSE_CURLY_BRACKET_TK = 22
     # Comments
     POUND_TK = 23
     # Bound Words
@@ -82,12 +100,57 @@ class TokenType:
     EOF_TK = 45
 
 
+tokens = {'+': TokenType.PLUS_TK,
+          '-': TokenType.MINUS_TK,
+          '*': TokenType.TIMES_TK,
+          '/': TokenType.DIV_TK,
+          '<': TokenType.LESS_TK,
+          '>': TokenType.GREATER_TK,
+          '=': TokenType.EQUAL_TK,
+          '<=': TokenType.LESS_OR_EQUAL_TK,
+          '>=': TokenType.GREATER_OR_EQUAL_TK,
+          '<>': TokenType.NOT_EQUAL,
+          ':': TokenType.COLON_TK,
+          ':=': TokenType.ASSIGN_TK,
+          ';': TokenType.SEMI_COLON_TK,
+          ',': TokenType.COMMA_TK,
+          '[': TokenType.OPEN_SQUARE_BRACKET_TK,
+          ']': TokenType.CLOSE_SQUARE_BRACKET_TK,
+          '(': TokenType.OPEN_PARENTHESIS_TK,
+          ')': TokenType.CLOSE_PARENTHESIS_TK,
+          '{': TokenType.OPEN_CURLY_BRACKET_TK,
+          '}': TokenType.CLOSE_CURLY_BRACKET_TK,
+          '.': TokenType.PERIOD_TK,
+          '#': TokenType.POUND_TK,
+          'program': TokenType.PROGRAM_TK,
+          'declare': TokenType.DECLARE_TK,
+          'if': TokenType.IF_TK,
+          'else': TokenType.ELSE_TK,
+          'while': TokenType.WHILE_TK,
+          'switchcase': TokenType.SWITCHCASE_TK,
+          'forcase': TokenType.FORCASE_TK,
+          'incase': TokenType.INCASE_TK,
+          'case': TokenType.CASE_TK,
+          'default': TokenType.DEFAULT_TK,
+          'not': TokenType.NOT_TK,
+          'and': TokenType.AND_TK,
+          'or': TokenType.OR_TK,
+          'function': TokenType.FUNCTION_TK,
+          'procedure': TokenType.PROCEDURE_TK,
+          'call': TokenType.CALL_TK,
+          'return': TokenType.RETURN_TK,
+          'in': TokenType.IN_TK,
+          'inout': TokenType.INOUT_TK,
+          'input': TokenType.INPUT_TK,
+          'print': TokenType.PRINT_TK, }
+
+
 #                                #
 # --------Lexical Analyzer-------#
 #                                #
 def lex():
     global input_file, char_number, line_number
-    number_limit = 2 ** 32 - 1
+    number_limit = 2**32 - 1
     char = input_file.read(1)
     char_number += 1
 
@@ -102,64 +165,97 @@ def lex():
         # Init tokenString.
         tokenString = char
 
+        # Number
         if char.isdigit():
             while char.isdigit():
                 char = input_file.read(1)
                 if char.isalpha():
                     print("Error_1 in line ", line_number, " char_number: ", char_number)  # MAKE A Def()
-                    break
+                    exit()
                 elif char.isdigit():
                     char_number += 1
                     tokenString += char
             if int(tokenString) < -number_limit or int(tokenString) > number_limit:
                 print("Error_2: Number <> 2^32 - 1")  # MAKE A Def()
+                exit()
             input_file.seek(input_file.tell() - 1)
-            print(tokenString)  # RETURN
+            return Token(TokenType.NUM_TK, tokenString, line_number, char_number)
+        # Identifier or Keyword
         elif char.isalpha():
             while char.isalpha() or char.isdigit():
                 if len(tokenString) > 30:
-                    print("Error_3 | MORE THAN 30 | in line ", line_number, " char_number: ", char_number)  # MAKE A Def()
-                    break
+                    print("Error_3 | MORE THAN 30 | in line ", line_number, " char_number: ",
+                          char_number)  # MAKE A Def()
+                    exit()
                 char = input_file.read(1)
                 if char.isalpha() or char.isdigit():
                     char_number += 1
                     tokenString += char
             input_file.seek(input_file.tell() - 1)  # Check Bound Words !!!
-            print(tokenString)  # RETURN
+            if tokenString in tokens:
+                return Token(tokens[tokenString], tokenString, line_number, char_number)
+            else:
+                return Token(TokenType.ID_TK, tokenString, line_number, char_number)
+
+        # Arithmetic Operators
         elif char == '+':
-            tokenString += char
-            print(tokenString)  # RETURN
-            return tokenString
+            return Token(TokenType.PLUS_TK, tokenString, line_number, char_number)
         elif char == '-':
-            tokenString += char
-            return tokenString
-        elif char == '>':
-            tokenString += char
+            return Token(TokenType.MINUS_TK, tokenString, line_number, char_number)
+        elif char == '*':
+            return Token(TokenType.TIMES_TK, tokenString, line_number, char_number)
+        elif char == '/':
+            return Token(TokenType.DIV_TK, tokenString, line_number, char_number)
+        # Grouping Symbols
+        elif char == '{':
+            return Token(TokenType.OPEN_CURLY_BRACKET_TK, tokenString, line_number, char_number)
+        elif char == '}':
+            return Token(TokenType.CLOSE_CURLY_BRACKET_TK, tokenString, line_number, char_number)
+        elif char == '[':
+            return Token(TokenType.OPEN_SQUARE_BRACKET_TK, tokenString, line_number, char_number)
+        elif char == ']':
+            return Token(TokenType.CLOSE_SQUARE_BRACKET_TK, tokenString, line_number, char_number)
+        elif char == '(':
+            return Token(TokenType.OPEN_PARENTHESIS_TK, tokenString, line_number, char_number)
+        elif char == ')':
+            return Token(TokenType.CLOSE_PARENTHESIS_TK, tokenString, line_number, char_number)
+        # Delimiter
+        elif char == ',':
+            return Token(TokenType.COMMA_TK, tokenString, line_number, char_number)
+        elif char == ';':
+            return Token(TokenType.SEMI_COLON_TK, tokenString, line_number, char_number)
+        # Assignment
+        elif char == ':':
             char = input_file.read(1)
             if char == '=':
                 tokenString += char
                 char_number += 1
-                return tokenString
+                return Token(TokenType.ASSIGN_TK, tokenString, line_number, char_number)
             input_file.seek(input_file.tell() - 1)
-            return tokenString
-        elif char == '<':
-            tokenString += char
+            return Token(TokenType.PERIOD_TK, tokenString, line_number, char_number)
+        # Relation Operators
+        elif char == '>':
             char = input_file.read(1)
             if char == '=':
                 tokenString += char
                 char_number += 1
-                print(tokenString)
-                return tokenString
+                return Token(TokenType.GREATER_OR_EQUAL_TK, tokenString, line_number, char_number)
+            input_file.seek(input_file.tell() - 1)
+            return Token(TokenType.GREATER_TK, tokenString, line_number, char_number)
+        elif char == '<':
+            char = input_file.read(1)
+            if char == '=':
+                tokenString += char
+                char_number += 1
+                return Token(TokenType.LESS_OR_EQUAL_TK, tokenString, line_number, char_number)
             elif char == '>':
                 tokenString += char
                 char_number += 1
-                print(tokenString)
-                return tokenString
+                return Token(TokenType.NOT_EQUAL, tokenString, line_number, char_number)
             input_file.seek(input_file.tell() - 1)
-            print(tokenString)
-            return tokenString
-
-
+            return Token(TokenType.LESS_TK, tokenString, line_number, char_number)
+        elif char == '=':
+            return Token(TokenType.EQUAL_TK, tokenString, line_number, char_number)
 
 
 # -------------------------------- #
@@ -167,9 +263,14 @@ def lex():
 # -------------------------------- #
 def error():
     pass
+
+
 # -------------------------------- #
 # |             Main             | #
 # -------------------------------- #
+token = Token()
+
+
 def main(input_Cimple_file):
     global input_file
     input_file = open(input_Cimple_file, 'r')
