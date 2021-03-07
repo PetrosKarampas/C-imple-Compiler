4f9602673f34f8cd734167846b2478dabf6f93a3
PetrosKarampas <petros6_97@yahoo.gr>
2021-03-07 21:26:08 +0200

    expand syntax analyzer

--- Cimple.py
+++ Cimple.py
@@ -1,12 +1,5 @@
 import sys
 
-# ---------------------------------------#
-# -          Global Variables           -#
-# ---------------------------------------#
-input_file = None
-char_number = 0
-line_number = 0
-
 
 class Token:
     def __init__(self, tk_type=None, tk_string=None, tk_line_number=None, tk_char_number=None):
@@ -144,6 +137,15 @@ tokens = {'+': TokenType.PLUS_TK,
           'input': TokenType.INPUT_TK,
           'print': TokenType.PRINT_TK, }
 
+# ---------------------------------------#
+# -          Global Variables           -#
+# ---------------------------------------#
+input_file = None
+char_number = 0
+line_number = 1
+token = Token(None, None, None, None)
+has_return = False
+
 
 #                                #
 # --------Lexical Analyzer-------#
@@ -170,28 +172,32 @@ def lex():
             while char.isdigit():
                 char = input_file.read(1)
                 if char.isalpha():
-                    error("Variable names cannot start with numbers",line_number,char_number)
+                    error("Variable names cannot start with numbers", line_number, char_number)
                 elif char.isdigit():
                     char_number += 1
                     tokenString += char
             if int(tokenString) < -number_limit or int(tokenString) > number_limit:
-                error("Number can only be less than or greater than 2^32 - 1",line_number,char_number)
+                error("Number can only be less than or greater than 2^32 - 1", line_number, char_number)
             input_file.seek(input_file.tell() - 1)
             return Token(TokenType.NUM_TK, tokenString, line_number, char_number)
-        
+
         # Identifier or Keyword
         elif char.isalpha():
             while char.isalpha() or char.isdigit():
                 if len(tokenString) > 30:
-                    error("More than 30",line_number,char_number)
+                    error("More than 30", line_number, char_number)
                 char = input_file.read(1)
                 if char.isalpha() or char.isdigit():
                     char_number += 1
                     tokenString += char
             input_file.seek(input_file.tell() - 1)
-            if tokenString in tokens: # Check Bound Words
+
+            if tokenString in tokens:
+                token.set_token_type(tokens[tokenString])
+                # Check Bound Words
                 return Token(tokens[tokenString], tokenString, line_number, char_number)
             else:
+                token.set_token_type(TokenType.ID_TK)
                 return Token(TokenType.ID_TK, tokenString, line_number, char_number)
 
         # Arithmetic Operators
@@ -203,7 +209,7 @@ def lex():
             return Token(TokenType.TIMES_TK, tokenString, line_number, char_number)
         elif char == '/':
             return Token(TokenType.DIV_TK, tokenString, line_number, char_number)
-        
+
         # Grouping Symbols
         elif char == '{':
             return Token(TokenType.OPEN_CURLY_BRACKET_TK, tokenString, line_number, char_number)
@@ -217,13 +223,13 @@ def lex():
             return Token(TokenType.OPEN_PARENTHESIS_TK, tokenString, line_number, char_number)
         elif char == ')':
             return Token(TokenType.CLOSE_PARENTHESIS_TK, tokenString, line_number, char_number)
-        
+
         # Delimiter
         elif char == ',':
             return Token(TokenType.COMMA_TK, tokenString, line_number, char_number)
         elif char == ';':
             return Token(TokenType.SEMI_COLON_TK, tokenString, line_number, char_number)
-        
+
         # Assignment
         elif char == ':':
             char = input_file.read(1)
@@ -233,8 +239,8 @@ def lex():
                 return Token(TokenType.ASSIGN_TK, tokenString, line_number, char_number)
             else:
                 input_file.seek(input_file.tell() - 1)
-                error("Expected '=' after ':'",line_number,char_number)
-        
+                error("Expected '=' after ':'", line_number, char_number)
+
         # Relation Operators
         elif char == '>':
             char = input_file.read(1)
@@ -258,7 +264,7 @@ def lex():
             return Token(TokenType.LESS_TK, tokenString, line_number, char_number)
         elif char == '=':
             return Token(TokenType.EQUAL_TK, tokenString, line_number, char_number)
-        
+
         # Comments
         elif char == '#':
             while char:
@@ -272,42 +278,174 @@ def lex():
                 if char == '#':
                     break
                 elif char == '':
-                    error("EOF. Comments never closed",line_number,char_number)
+                    error("EOF. Comments never closed", line_number, char_number)
             char = input_file.read(1)
-        
+
         elif char == '':
             return Token(TokenType.EOF_TK, 'EOF', line_number, 0)
         elif char == '.':
             return Token(TokenType.PERIOD_TK, '.', line_number, char_number)
         else:
-            error("Illegal Character",line_number,char_number)
+            error("Illegal Character", line_number, char_number)
+
 
 # -------------------------------- #
 # |             Error            | #
 # -------------------------------- #
-def error(error_message,line_number,char_number):
+def error(error_message, line_number, char_number):
     global input_file
-    print(input_file.name,"::","Line:",line_number+1,"::","Char:",char_number,"::","ERROR:",error_message)
+    print(input_file.name, "::", "Line:", line_number, "::", "Char:", char_number, "::", "ERROR:", error_message)
     input_file.seek(0)
     input_file.close()
     exit(1)
 
 
+# -------------------------------- #
+# |        Syntax Analyzer       | #
+# -------------------------------- #
+def program():
+    global token, line_number, char_number
+    if token.tk_type is TokenType.PROGRAM_TK:
+        print(token.tk_string)  # Check
+        token = lex()
+
+        if token.tk_type is TokenType.ID_TK:
+            program_name = token.tk_string
+            print(program_name)
+            token = lex()
+            print(token.tk_string)
+            programblock()
+        else:
+            error('Program name \'%s\' is not valid.' % token.tk_string, line_number, char_number)
+    else:
+        error('Expected \'program\' keyword instead of \'%s\' .' % token.tk_string, line_number, char_number)
+
+
+def programblock():
+    declarations()
+    subprograms()
+    block()
+
+
+def declarations():
+    global token
+    while token.tk_type == TokenType.DECLARE_TK:
+        token = lex()
+        print(token.tk_string)
+        vardecl()
+        if token.tk_type != TokenType.SEMI_COLON_TK:
+            error('Expected \';\' after declaration of variables.', line_number, char_number)
+        token = lex()
+        print(token.tk_string)
+
+
+def vardecl():
+    global token
+    if token.tk_type is TokenType.ID_TK:
+        token = lex()
+        print(token.tk_string)
+        while token.tk_type is TokenType.COMMA_TK:
+            token = lex()
+            print(token.tk_string)
+            if token.tk_type is not TokenType.ID_TK:
+                error("Expected Comma", line_number, char_number)
+        token = lex()
+        print(token.tk_string)
+
+
+def subprograms():
+    global token
+    while token.tk_type is TokenType.PROCEDURE_TK or token.tk_type is TokenType.FUNCTION_TK:
+        if token.tk_type is TokenType.PROCEDURE_TK:
+            token = lex()
+            print(token.tk_string)
+            subprogram()
+        elif token.tk_type is TokenType.FUNCTION_TK:
+            token = lex()
+            print(token.tk_string)
+            subprogram()
+
+
+def subprogram():
+    global token
+    if token.tk_type is TokenType.ID_TK:
+        token = lex()
+        print(token.tk_string)
+        if token.tk_type is TokenType.OPEN_PARENTHESIS_TK:
+            token = lex()
+            print(token.tk_string)
+            formalparlist()
+            token = lex()
+            print(token.tk_string)
+        else:
+            error('Expected \'(\' instead found: %s' % token.tk_string, line_number, char_number)
+        if token.tk_type is TokenType.CLOSE_PARENTHESIS_TK:
+            token = lex()
+            print(token.tk_string)
+            programblock()
+        else:
+            error('Expected \')\' instead found: %s' % token.tk_string, line_number, char_number)
+    else:
+        error('Expected ID instead of: %s' % token.tk_string, line_number, char_number)
+
+
+def formalparlist():
+    global token
+    pass
+
+
+def formalparlitem():
+    global token
+    if token.tk_type is TokenType.IN_TK:
+        token = lex()
+        print(token.tk_string)
+        if token.tk_type is not TokenType.ID_TK:
+            error('Expected ID instead of: %s' % token.tk_string, line_number, char_number)
+    else:
+        error('Expected \'in.', line_number, char_number)
+
+    if token.tk_type is TokenType.INOUT_TK:
+        token = lex()
+        print(token.tk_string)
+        if token.tk_type is not TokenType.ID_TK:
+            error('Expected ID instead of: %s' % token.tk_string, line_number, char_number)
+    else:
+        error('Expected \'inout.', line_number, char_number)
+
+
+def block():
+    global token
+    if token.tk_type == TokenType.OPEN_CURLY_BRACKET_TK:
+        token = lex()
+        print(token.tk_string)
+        sequence()
+        if token.tk_type == TokenType.CLOSE_CURLY_BRACKET_TK:
+            token = lex()
+            print(token.tk_string)
+        else:
+            error("Block was not closed. '}' was expected", line_number, char_number)
+    else:
+        error("Block was not opened. '{' was expected.", line_number, char_number)
+
+
+def sequence():
+    pass
+
 
 # -------------------------------- #
 # |             Main             | #
 # -------------------------------- #
-token = Token()
-
 
 def main(input_Cimple_file):
     global input_file
+    global token
+
     input_file = open(input_Cimple_file, 'r')
-
-
+    # Initiate Syntax Analysis
     token = lex()
+    program()
     while token.tk_type is not TokenType.PERIOD_TK:
-        print(token.tk_string, "|Type: ", token.tk_type)
+        # print(token.tk_string, "|Type: ", token.tk_type)
         token = lex()
 
 
